fa9ef8fb111085348aed4c014306c833cbb0bd87
LE Cong Tuan <congtuanle23@gmail.com>
2022-07-23 18:03:19 +0200

    feat(options): handle options request and add headers

--- src/listener.ts
+++ src/listener.ts
@@ -6,10 +6,18 @@ import postReqHandler from './handlers/post';
 // function that return a request listener function
 export default function (dataSrc: { [key: string]: any }, jsonPath: string) {
   return function (req: IncomingMessage, res: ServerResponse) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,POST,PUT,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Max-Age', '86400');
+
     const method = req.method ?? '';
     logger(method, req.url ?? '');
 
-    if (method === 'GET') {
+    if (method === 'OPTIONS') {
+      res.writeHead(204);
+      return res.end();
+    } else if (method === 'GET') {
       return getReqHandler(req, res, dataSrc);
     } else if (method === 'POST') {
       return postReqHandler(req, res, dataSrc, jsonPath);
